6e9493413575e3d7d167211b4c5861cd7b208db3
Bernard Teo <btzy1996@hotmail.com>
2017-02-10 13:48:08 +0800

    no compress and mangle - simply combine to one file

--- uglify-old.bat
+++
@@ -1,1 +0,0 @@
-uglifyjs FontLoader.js key-polyfill.js encoding-polyfill.js findindex-polyfill.js find-polyfill.js objects.js interpolator.js changelimited.js analytics.js dom.js main.js facebook.js twitter.js --mangle toplevel --compress sequences,unsafe,comparisons,unsafe_comps,pure_getters,collapse_vars,reduce_vars,keep_fargs=false,passes=5 --screw-ie8 --output celestia.min.js
